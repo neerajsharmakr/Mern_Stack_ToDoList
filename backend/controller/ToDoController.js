3bf1d33679115879f7167688782f7dd7f4970ae6
neerajsharmakr047@gmail.com <neerajsharmakr047@gmail.com>
2023-09-16 23:03:29 +0530

    update files

--- backend/controller/ToDoController.js
+++ backend/controller/ToDoController.js
@@ -7,12 +7,18 @@ module.exports.getToDo = async (req, res) => {
 
 module.exports.saveToDo = async (req, res) => {
   const { text } = req.body;
-
-  ToDoModel.create({ text }).then((data) => {
-    console.log("Added Successfully");
-    console.log(data);
-    res.send(data);
-  });
+  ToDoModel.create({ text })
+    .then((resp) => {
+      console.log("Added Successfully");
+      res.status(200).json({
+        status: true,
+        message: "Added Successfully",
+        data: resp,
+      });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 };
 
 module.exports.updateToDo = async (req, res) => {
