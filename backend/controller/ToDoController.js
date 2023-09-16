57a8d410767449d2fb6a7c86fc8dc3053f05e848
neerajsharmakr047@gmail.com <neerajsharmakr047@gmail.com>
2023-09-16 15:15:36 +0530

    add frontend and backend

---
+++ backend/controller/ToDoController.js
@@ -0,0 +1,30 @@
+const ToDoModel = require("../model/ToDoModel");
+
+module.exports.getToDo = async (req, res) => {
+  const todo = await ToDoModel.find();
+  res.send(todo);
+};
+
+module.exports.saveToDo = async (req, res) => {
+  const { text } = req.body;
+
+  ToDoModel.create({ text }).then((data) => {
+    console.log("Added Successfully");
+    console.log(data);
+    res.send(data);
+  });
+};
+
+module.exports.updateToDo = async (req, res) => {
+  const { _id, text } = req.body;
+  ToDoModel.findByIdAndUpdate(_id, { text })
+    .then(() => res.send("Update succesfully..."))
+    .catch((err) => console.log(err));
+};
+
+module.exports.deleteToDo = async (req, res) => {
+  const { _id } = req.body;
+  ToDoModel.findByIdAndDelete(_id)
+    .then(() => res.send("Delete succesfully..."))
+    .catch((err) => console.log(err));
+};
